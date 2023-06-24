012f343ef73f5d4852bb445b9a09674b300adb46
danhumassmed <daniel.higgins@umassmed.edu>
2023-06-24 12:07:12 -0400

    Updated build to 1.0.1

--- docker/fastqc-multiqc/build.sh
+++ docker/fastqc-multiqc/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USER="danhumassmed"
 TAG="fastqc-multiqc"
-VERSION="1.0.0"
+VERSION="1.0.1"
 echo "********************************************"
 echo docker buildx build --platform linux/amd64,linux/arm64 --push -t ${USER}/${TAG}:${VERSION} .
--- docker/star-rsem/build.sh
+++ docker/star-rsem/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 USER="danhumassmed"
 TAG="star-rsem"
-VERSION="1.0.0"
-echo docker buildx build --platform linux/amd64,linux/arm64 --push -t ${USER}/${TAG}:${VERSION} .
+VERSION="1.0.1"
+echo docker buildx build --platform linux/amd64 --push -t ${USER}/${TAG}:${VERSION} .
