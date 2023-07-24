87479bb5a669186f6fb05aa36a33e499826f8b52
danhumassmed <daniel.higgins@umassmed.edu>
2023-07-24 11:48:19 -0400

    updates for documentation

--- utility/start_debrowser.sh
+++ utility/start_debrowser.sh
@@ -7,6 +7,6 @@ email="${email}@umassmed.edu"
 current_user=`echo ${LOGNAME}@hpc.umassmed.edu`
 
 echo "Starting ..."
-echo -e "Hello\n\nYour DEBrowser session has started on the UMass HPC and will run for 2 Hours.\n\nYou can connect from UMass Chan Open OnDemand SCI Desctop using the following link http://${host_ip}:8088\n\nor create an SSH tunnel with this command 'ssh -L 8088:${host_ip}:8088 ${current_user}'\n\nand connect using your local pc at 127.0.0.1:8088"|/usr/bin/mail -s 'DEBrowser started on HPC' ${email}
+echo -e "Hello\n\nYour DEBrowser session has started on the UMass HPC.\n\nYou can connect from UMass Chan Open OnDemand SCI Desctop using the following link http://${host_ip}:8088\n\nor create an SSH tunnel with this command 'ssh -L 8088:${host_ip}:8088 ${current_user}'\n\nand connect using your local pc at 127.0.0.1:8088"|/usr/bin/mail -s 'DEBrowser started on HPC' ${email}
 
 exec singularity exec --pid ${HOME}/.singularity/danhumassmed-debrowser-1.0.1.img Rscript /startDEBrowser.R
