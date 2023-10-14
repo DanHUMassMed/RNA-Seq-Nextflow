c91d491072a3b7b2bcfcbe3e89001074ec388a74
danhumassmed <daniel.higgins@umassmed.edu>
2023-10-14 13:36:22 -0400

    moved shared utilities

--- pipelines/shared/utility/wormbase_download.sh
+++ pipelines/shared/utility/wormbase_download.sh
@@ -2,13 +2,14 @@
 # Select Version from https://wormbase.org/ 
 wormbase_version="WS289"
 
-launch_dir="/home/daniel.higgins-umw/project_data/RNA-Seq-Nextflow"
+launch_dir="/home/${USER}/project_data/RNA-Seq-Nextflow"
 base_dir="${launch_dir}/pipelines/shared/data"
 
-base_url="ftp://ftp.wormbase.org/pub/wormbase/releases/${wormbase_version}/species/c_elegans/PRJNA13758/"
+base_url="ftp://ftp.wormbase.org/pub/wormbase/releases/${wormbase_version}/species/c_elegans/PRJNA13758"
 genes_fasta="c_elegans.PRJNA13758.${wormbase_version}.genomic.fa.gz"
 transcripts_fasta="c_elegans.PRJNA13758.${wormbase_version}.mRNA_transcripts.fa.gz"
 annotations_gtf="c_elegans.PRJNA13758.${wormbase_version}.canonical_geneset.gtf.gz"
+gene_ids="c_elegans.PRJNA13758.${wormbase_version}.geneIDs.txt.gz"
 
 mkdir -p ${base_dir}
 cd ${base_dir}
@@ -16,6 +17,8 @@ cd ${base_dir}
 wget -nv ${base_url}/${genes_fasta}
 wget -nv ${base_url}/${transcripts_fasta}
 wget -nv ${base_url}/${annotations_gtf}
-gunzip ${genes_fasta}
-gunzip ${transcripts_fasta}
-gunzip ${annotations_gtf}
+wget -nv ${base_url}/annotation/${gene_ids}
+gunzip --force ${genes_fasta}
+gunzip --force ${transcripts_fasta}
+gunzip --force ${annotations_gtf}
+gunzip --force ${gene_ids}
