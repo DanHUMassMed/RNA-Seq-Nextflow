3d0e1c3557d0c3dae52ac2f1972be9206b29ff4e
danhumassmed <daniel.higgins@umassmed.edu>
2023-10-31 15:19:06 -0400

    added run code for DESeq2

---
+++ docker/de-seq-tools/call-deseq.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Used to test new features added to deseq-tools
+mount_dir="/Users/dan/Code/NextFlow/RNA-Seq-Nextflow/docker/de-seq-tools"
+input_file="genes_expression_expected_count.tsv"
+output_dir="deseq_out"
+
+echo "Running DESeq container at ${mount_dir} and cvs path ${input_csv_dir}"
+
+# docker run --platform linux/amd64 --rm -v ${mount_dir}:/usr/data danhumassmed/de-seq-tools:1.0.1 \
+#       Rscript /usr/data/low_counts_filter.R \
+#       -i /usr/data/${input_file} \
+#       -o /usr/data/${output_dir} \
+#       -l 10
+
+input_file="./deseq_out/count_data_low_counts_filtered.tsv"
+docker run --platform linux/amd64 --rm -v ${mount_dir}:/usr/data danhumassmed/de-seq-tools:1.0.1 \
+      Rscript /usr/data/run_deseq2.R \
+      -i /usr/data/${input_file} \
+      -o /usr/data/${output_dir} \
+      -m /usr/data/run_oxIs12_ABC16.csv
+
+echo "The results can be found here:  ${mount_dir}/${output_dir}"
+
+
