4a57709c2d830f152cff1dbf2a67f9ebfc50f192
danhumassmed <daniel.higgins@umassmed.edu>
2023-11-01 21:41:21 -0400

    updates to deseq run

--- docker/de-seq-tools/build.sh
+++ docker/de-seq-tools/build.sh
@@ -7,7 +7,7 @@ echo "********************************************"
 echo ../push_description.py -u \"${USER}\" -i ${USER}/${TAG} -r README.md -s \"${SHORT_DESC}\"
 
 echo "********************************************"
-echo docker buildx build --platform linux/amd64,linux/arm64 --push -t ${USER}/${TAG}:${VERSION} .
+echo docker buildx build --platform linux/amd64 --push -t ${USER}/${TAG}:${VERSION} .
 
 echo "********************************************"
 echo docker build -t ${USER}/${TAG}:${VERSION} .
